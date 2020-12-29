31e22e5652baa41513aafe28148ceb63cd2b90b3
ashish yadav <ashishyadav.mnnit@gmail.com>
2020-12-29 15:29:51 +0000

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,18 @@ Initial Function()
 Initial Security()
 {
  //Sample Code
+  {
+
+ feature1()
+ {
+   //Sample Code for Feature 1
+ }
+
+
+ feaature2()
+ {
+   //Sample Code for Feature 2
+ }
 }
 
 
